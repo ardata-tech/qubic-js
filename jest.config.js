7fb90247d862736a6ec6970611cb10c04c3ba344
Alexander Paul Quinit <paulquinit@gmail.com>
2025-02-16 17:13:43 +0800

    Update Jest configuration to use ES module syntax and add support for TypeScript extensions

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
-export const preset = "ts-jest";
-export const testEnvironment = "node";
-export const testMatch = ["<rootDir>/test/**/*.test.ts"];
-export const clearMocks = true;
-export const coverageDirectory = "coverage";
-export const collectCoverageFrom = ["src/**/*.ts"];
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/test/**/*.test.ts"],
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.ts"],
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  transform: {}
+};
