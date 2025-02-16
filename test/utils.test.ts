aa3ec83ed44f995082244c9adef12ea086511d31
Alexander Paul Quinit <paulquinit@gmail.com>
2025-02-16 10:32:18 +0800

    Remove redundant test for seed conversion in Utils module

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -7,12 +7,4 @@ describe("Utils Module", () => {
     expect(seed).toHaveLength(55);
     expect(seed).toMatch(/^[a-z]{55}$/);
   });
-
-  test("should convert a seed to bytes", () => {
-    const seed = Utils.createSeed();
-    const bytes = Utils.seedToBytes(seed);
-    expect(bytes).toBeDefined();
-    expect(bytes).toBeInstanceOf(Uint8Array);
-    expect(bytes).toHaveLength(seed.length);
-  });
 });
