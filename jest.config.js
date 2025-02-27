531dab9add7153d4832457386136c355620801b3
Alexander Paul Quinit <paulquinit@gmail.com>
2025-02-27 20:28:52 +0800

    feat: add test coverage configuration with Coveralls integration

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  transformIgnorePatterns: ["/node_modules/"],
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
 };
